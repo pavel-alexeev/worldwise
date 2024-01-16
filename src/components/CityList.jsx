9a5671bf4c24fc600c9fde6b96808f235dd5ba76
Pavels Aleksejevs <paul.alexeev@yahoo.com>
2024-01-16 22:54:06 +0000

    initial commit

---
+++ src/components/CityList.jsx
@@ -0,0 +1,28 @@
+import Spinner from "./Spinner";
+import styles from "./CityList.module.css";
+import CityItem from "./CityItem";
+import Message from "./Message";
+
+import PropTypes from "prop-types";
+
+export default function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
+}
+
+CityList.propTypes = {
+  cities: PropTypes.array.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+};
